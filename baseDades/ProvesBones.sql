968e527bee0c8a3fce1dc2dad272257c67facafb
joan-pons <ptj@paucasesnovescifp.cat>
2020-04-23 14:13:41 +0200

    Aplicació a subdirectori recuperar contrasenya Alta alumne Filtrar alumnes en validar Dump Falta  - autenticació professors a google.  - Ajuda

--- baseDades/ProvesBones.sql
+++ baseDades/ProvesBones.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `borsa` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `borsa`;
 -- MySQL dump 10.13  Distrib 5.7.29, for Linux (x86_64)
 --
 -- Host: localhost    Database: borsa
@@ -1077,4 +1079,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-04-23 11:13:11
+-- Dump completed on 2020-04-23 13:08:09
